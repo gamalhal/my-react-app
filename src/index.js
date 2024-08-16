a834138def65c2e23fdeb4adf81fc3a4bed49ede
Gamal Hal <gamalhal.chi@gmail.com>
2024-08-16 22:52:57 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Assuming you have an App component
+
+ReactDOM.render(<App />, document.getElementById('root'));
